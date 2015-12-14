9dfc056f5c2c191e34d8386cbc20515c5b717a4b
Jeff Wagner <jwagner@thoughtworks.com>
2015-12-13 20:01:14 -0800

    added user table and added user_id to programs

--- app/models/program.rb
+++ app/models/program.rb
@@ -2,4 +2,5 @@ class Program < ActiveRecord::Base
   validates :program_name, presence: true, length: { minimum: 1, maximum: 25 }
   has_many :exercise_programs
   has_many :exercises, through: :exercise_programs
+  belongs_to :user
 end
